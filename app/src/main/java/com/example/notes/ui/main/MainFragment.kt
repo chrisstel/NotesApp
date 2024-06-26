ca09b7074edc270184074a796ce8172a09e2ebb7
pelmenman <jashinisso_job@mail.ru>
2024-06-26 15:17:53 +0300

    Refactoring

--- app/src/main/java/com/example/notes/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/notes/ui/main/MainFragment.kt
@@ -1,8 +1,8 @@
 package com.example.notes.ui.main
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
